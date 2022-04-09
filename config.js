cee3651005c3d32fc6e34604cd6133642d4de666
maycol125 <77649026+maycol125@users.noreply.github.com>
2022-04-09 08:48:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     opt: {
         DJ: {
-            enabled: false, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
+            enabled: true, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
